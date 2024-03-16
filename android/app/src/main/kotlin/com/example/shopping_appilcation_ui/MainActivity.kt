9419e7a60fe57d8c771ad3fdfe2b328c43cd48d5
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-16 15:11:27 +0800

    Starter code

---
+++ android/app/src/main/kotlin/com/example/shopping_appilcation_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shopping_appilcation_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
