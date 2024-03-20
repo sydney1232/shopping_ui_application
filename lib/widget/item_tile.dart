a91ca1b9e69781ce0499a1432f7188fc6398bcbb
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-20 21:14:30 +0800

    Implemetation Item Tile

--- lib/widget/item_tile.dart
+++ lib/widget/item_tile.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../categories.dart';
 
 class ItemTile extends StatefulWidget {
-  const ItemTile({super.key});
+  final String image;
+  final Categories category;
+  final String brand;
+  final double price;
+
+  ItemTile(
+      {super.key,
+      required this.image,
+      required this.category,
+      required this.brand,
+      required this.price});
 
   @override
   State<ItemTile> createState() => _ItemTileState();
@@ -10,13 +23,83 @@ class ItemTile extends StatefulWidget {
 class _ItemTileState extends State<ItemTile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 250,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        image: DecorationImage(
-            image: AssetImage('assets/images/one.jpg'), fit: BoxFit.cover),
+    return Hero(
+      tag: 'shoe',
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.only(bottom: 20, right: 10, left: 10),
+          height: 250,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 10),
+                blurRadius: 10,
+                color: Colors.grey,
+              ),
+            ],
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              image: AssetImage(widget.image),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.category.name,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          widget.brand,
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 35,
+                    width: 35,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.favorite_border,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                widget.price.toDouble() % 1 == 0
+                    ? "${widget.price.toInt()}\$"
+                    : "${widget.price.toString()}\$",
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 38,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
